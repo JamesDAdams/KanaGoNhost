5b5a85329ffbfd7b260238f63d9fb1de5af75832
JamesAdams <jamesadamspub@gmail.com>
2025-03-23 16:11:49 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,7 @@
 export default (req, res) => {
+  console.log('Headers:', req.headers)
+  console.log('Auth:', req.auth)
+  
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
