598877e7d8fed2c8ff18cc43fb27507359a389b9
JamesAdams <jamesadamspub@gmail.com>
2025-03-23 15:57:26 +0100

    Update index.ts

--- functions/hello-world/index.ts
+++ functions/hello-world/index.ts
@@ -10,5 +10,13 @@ curl http://localhost:1337/v1/functions/hello-world
 import { Request, Response } from 'express'
 
 export default (req: Request, res: Response) => {
-  res.status(200).send(`Hello world`)
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end()
+  }
+
+  res.status(200).send('Hello world')
 }
